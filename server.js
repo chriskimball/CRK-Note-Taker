32064c11f5275b3415548883efae11f08764f316
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-01-29 14:54:56 -0800

    Working delete function, also removed extra commenting/pseudo code

--- server.js
+++ server.js
@@ -7,7 +7,6 @@
 
 //Required modules
 const express = require('express');
-const { fstat } = require('fs');
 const path = require('path');
 const {v4: uuidv4} = require('uuid');
 const { readFile, writeFile } = require('./helpers/fsUtils')
@@ -20,8 +19,6 @@ uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const notes = require('./db/db.json');
-
 // Defining the front end directory.
 app.use(express.static('public'));
 
@@ -31,21 +28,12 @@ app.use(express.urlencoded({extended: true}));
 
 // Fetch notes request
 app.get('/notes', (req, res) => {
-
     // Send the file `notes.html`
     res.sendFile( path.join( __dirname, 'public/notes.html'))
 });
 
 app.get('/api/notes', (req, res) => {
-    
-    // readFile('./db/db.json')
-    
-    
-    // console.log("notesdata:",notes)
     readFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-    // Fetching db.json data
-    // res.json( fs.readFile('./db/db.json') )
-    // './db/feedback.json').then((data) => res.json(JSON.parse(data))
 });
 
 app.post('/api/notes', (req, res) => {
@@ -57,31 +45,42 @@ app.post('/api/notes', (req, res) => {
             title: title,
             text: text,
             id: uuidv4(),
-            };
-    console.log(newNote)
+        };
+    
+    readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(newNote);
+            writeFile('./db/db.json', JSON.stringify(parsedData, null, 4));
+        }
+    });
 
+    res.sendFile( path.join( __dirname, 'public/notes.html'))
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+
+    const params = req.params.id;
 
     readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
           console.error(err);
         } else {
-          const parsedData = JSON.parse(data);
-          parsedData.push(newNote);
-          writeFile('./db/db.json', JSON.stringify(parsedData, null, 4));
-        }
-      });
-    // // Create (persist) data
-        
-    // // Add on uuid function onto the note object to allow DELETE function (DAY 2 Activity 20)
+            const parsedData = JSON.parse(data);
 
-    // // Access the new note from `req`
-    
-    // // Push it to my existing list of notes
-
-    // // Write my updated notes list to the `db.json` file
-    
-    // res.json(/* note data */);
-
+            for (let [i, obj] of parsedData.entries()) {
+                if (obj.id == params) {
+                    
+                    parsedData.splice(i, 1);
+                    
+                    writeFile('./db/db.json', JSON.stringify(parsedData, null, 4));
+                };
+            };
+        };
+    });
+    res.sendFile( path.join( __dirname, 'public/notes.html'))
 });
 
 app.listen(PORT, () => {
